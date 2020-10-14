1c48b509c942aa73df84a2cc8b0628e8bbab2c12
sandy1110 <sandiig.1110@gmail.com>
2020-10-14 08:08:52 -0500

    se agrego autenticacion a operaciones avanzadas

--- server/routes/operacionesAv.route.js
+++ server/routes/operacionesAv.route.js
@@ -1,9 +1,10 @@
 'use strict'
 const express = require('express');
+const Authenticator = require("../middlewares/authenticator")
 const OperacionesAvController = require('../controllers/OperacionesAvController')
 var api = express.Router();
 
 
-api.get('/potencia', OperacionesAvController.potencia);
+api.get('/potencia', Authenticator.authenticate("jwt",{session:false}), OperacionesAvController.potencia);
 api.get('/raiz', OperacionesAvController.raiz);
 module.exports = api;
